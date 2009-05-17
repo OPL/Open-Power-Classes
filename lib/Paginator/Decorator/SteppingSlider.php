ec1018d51d5ff59e5cf7b044505c2011e681160e
eXtreme <jacek.jedrzejewski@gmail.com>
2009-05-17 19:01:13 +0000

    Coding standards update

--- lib/Paginator/Decorator/SteppingSlider.php
+++ lib/Paginator/Decorator/SteppingSlider.php
@@ -7,7 +7,7 @@
  * with this package in the file LICENSE. It is also available through
  * WWW at this URL: <http://www.invenzzia.org/license/new-bsd>
  *
- * Copyright (c) 2008 Invenzzia Group <http://www.invenzzia.org>
+ * Copyright (c) Invenzzia Group <http://www.invenzzia.org>
  * and other contributors. See website for details.
  *
  * $Id$
