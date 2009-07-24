d210dc5ee40c13586e00a8153a5700e6b14f3e2f
megawebmaster <megawebmaster@gmail.com>
2009-07-24 20:22:02 +0000

    Opc_Cache first build, working

--- lib/Cache.php
+++ lib/Cache.php
@@ -20,27 +20,59 @@ class Opc_Cache implements Opt_Caching_Interface
 {
 	/**
 	 * The view that the caching system is connected to.
+	 *
 	 * @var Opt_View
 	 */
 	private $_view;
 
 	/**
 	 * The Opc_Class object.
+	 *
 	 * @var Opc_Class
 	 */
 	private $_opc;
 
 	/**
+	 * Cache dir.
+	 * 
+	 * @var String
+	 */
+	private $_cacheDir = null;
+
+	/**
 	 * The caching extra key.
+	 *
 	 * @var String
 	 */
 	private $_key = null;
 
 	/**
 	 * The caching validity time.
+	 *
 	 * @var Integer
 	 */
-	private $_time = 3600;
+	private $_time = null;
+
+	/**
+	 * Cache file name.
+	 *
+	 * @var String
+	 */
+	private $_filename = null;
+
+	/**
+	 * Determines if file was read or not.
+	 *
+	 * @var Boolean
+	 */
+	private $_isReadAlready = false;
+
+	/**
+	 * File handle.
+	 *
+	 * @var Resource|Null
+	 */
+	private $_fileHandle = null;
 
 	/**
 	 * Creates a new caching object and connects to it.
@@ -78,6 +110,28 @@ class Opc_Cache implements Opt_Caching_Interface
 			throw new Opc_InvalidArgumentType_Exception(gettype($time), 'integer');
 		}
 	} // end setExpiryTime();
+
+	/**
+	 * Sets directory for cache files.
+	 * @param String $dir New directory
+	 */
+	public function setCacheDir($dir)
+	{
+		if($dir[strlen($dir)-1] != DIRECTORY_SEPARATOR)
+		{
+			$dir .= DIRECTORY_SEPARATOR;
+		}
+		
+		// Prevention against current directory changes in Apache
+		// which affects destructors. We avoid it by switching to the
+		// absolute path.
+
+		if(isset($_SERVER['SERVER_SOFTWARE']) && strpos($_SERVER['SERVER_SOFTWARE'], 'Apache') !== false)
+		{
+			$dir = realpath($dir).DIRECTORY_SEPARATOR;
+		}
+		$this->_cacheDir = $dir;
+	} // end setCacheDir();
 	
 	/**
 	 * Returns the current extra caching key.
@@ -94,8 +148,39 @@ class Opc_Cache implements Opt_Caching_Interface
 	 */
 	public function getExpiryTime()
 	{
+		if($this->_time === null)
+		{
+			$this->_time = $this->_opc->expiryTime;
+		}
 		return $this->_time;
 	} // end getExpiryTime();
+
+	/**
+	 * Returns cache directory.
+	 * @return String
+	 */
+	public function getCacheDir()
+	{
+		if($this->_cacheDir === null)
+		{
+			$this->_cacheDir = $this->_opc->cacheDir;
+		}
+		return $this->_cacheDir;
+	}
+
+	/**
+	 * Gets the cache filename with special key.
+	 *
+	 * @return String
+	 */
+	private function _getFilename()
+	{
+		if($this->_filename == null)
+		{
+			$this->_filename = $this->_key.'_'.$this->_view->getTemplate().'.cch';
+		}
+		return $this->_filename;
+	} // end _getFilename();
 
 	/**
 	 * Checks if the specified view is cached.
@@ -104,7 +189,48 @@ class Opc_Cache implements Opt_Caching_Interface
 	 */
 	public function isCached()
 	{
-
+		if(!$this->_isReadAlready)
+		{
+			$this->_isReadAlready == true;
+			$this->_fileHandle = @fopen($this->_cacheDir.$this->_getFilename(), 'r');
+			if($this->_fileHandle === false)
+			{
+				return false;
+			}
+			$header = fgets($this->_fileHandle);
+			if($header[0] == '<')
+			{
+				$header = str_replace(array('<'.'?cacheHeader ','?>'), '', $header);
+			}
+			$header = unserialize($header);
+			if(!is_array($header))
+			{
+				/* When header is not an array it means it is wrong, so file must be deleted
+				   because of that and because it could be bad or unauthorized changed */
+				unlink($this->_cacheDir.$this->_getFilename());
+				$this->_fileHandle = null;
+				return false;
+			}
+			if($header['timestamp'] < (time() - $header['expiry']))
+			{
+				/* When cache file is old it should be deleted ;) */
+				unlink($this->_cacheDir.$this->_getFilename());
+				$this->_fileHandle = null;
+				return false;
+			}
+			return true;
+		}
+		else
+		{
+			if($this->_fileHandle !== null)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
 	} // end isCached();
 
 	/**
@@ -112,7 +238,7 @@ class Opc_Cache implements Opt_Caching_Interface
 	 */
 	public function clear()
 	{
-
+		unlink($this->_cacheDir.$this->_getFilename());
 	} // end clear();
 
 	/**
@@ -123,7 +249,21 @@ class Opc_Cache implements Opt_Caching_Interface
 	 */
 	public function templateCacheStart(Opt_View $view)
 	{
-
+		if($this->isCached())
+		{
+			while(!feof($this->_fileHandle))
+			{
+				$buf = fgets($this->_fileHandle, 2048);
+				echo $buf;
+			}
+			return true;
+		}
+		else
+		{
+			ob_start();
+			ob_implicit_flush(false);
+			return false;
+		}
 	} // end templateCacheStart();
 
 	/**
@@ -134,6 +274,13 @@ class Opc_Cache implements Opt_Caching_Interface
 	 */
 	public function templateCacheStop(Opt_View $view)
 	{
-		
+		$header = array(
+			'timestamp' => time(),
+			'expire' => $this->getExpiryTime()
+		);
+		$header = '<'.'?cacheHeader '.serialize($header).'?>'."\n";
+
+		// TODO implement dynamic templates
+		file_put_contents($this->getCacheDir().$this->_getFileName, $header.ob_get_clean());
 	} // end templateCacheStop();
 } // end Opc_Cache;
